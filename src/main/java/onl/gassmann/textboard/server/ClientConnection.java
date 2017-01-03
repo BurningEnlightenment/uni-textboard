78146439462417657590d531124481b574b8ee57
Henrik S. Gaßmann <Henrik.Gassmann@adesso.de>
2017-01-03 06:11:52 +0100

    IMPLEMENT BASIC CLIENT LOOP AND EXIT HANDLING

--- src/main/java/onl/gassmann/textboard/server/ClientConnection.java
+++ src/main/java/onl/gassmann/textboard/server/ClientConnection.java
@@ -68,14 +68,85 @@ public class ClientConnection implements Runnable, Closeable
     @Override
     public void run()
     {
+        while (!connection.isInputShutdown())
+        {
+            final String instruction;
+            try
+            {
+                instruction = in.readLine();
+            }
+            catch (IOException exc)
+            {
+                // todo read exception handling
+                break;
+            }
+            int length = instruction.length();
+            if (length < 1)
+            {
+                writeError("Got an empty line instead of an instruction.");
+            }
 
+            switch (instruction.charAt(0))
+            {
+                // exit command
+                case 'X':
+                    if (length != 1)
+                    {
+                        // X followed by an argument
+                        writeError("the exit instruction (X) must not contain any arguments.");
+                        break;
+                    }
+                    LOGGER.info("received the exit command from" + remoteAddress);
+                    try
+                    {
+                        close();
+                    }
+                    catch (IOException exc)
+                    {
+                        LOGGER.warning("Failed to close client after receiving the exit command from "
+                                               + getRemoteAddress() + "; details:\n" + exc.getMessage());
+                    }
+                    break;
+
+                // message post command
+                case 'P':
+                    writeError("the post command is not yet implemented.");
+                    break;
+
+                // last changed topic command
+                case 'L':
+                    writeError("the list command is not yet implemented.");
+                    break;
+
+                // get messages by topic
+                case 'T':
+                    writeError("the topic command is not yet implemented.");
+                    break;
+
+                // get all message newer than the specified time point
+                case 'W':
+                    writeError("the retrieve command is not yet implemented.");
+                    break;
+            }
+        }
+        try
+        {
+            connection.close();
+        }
+        catch (IOException exc)
+        {
+            LOGGER.severe("Failed to close the socket for " + getRemoteAddress() + "; details:\n" + exc.getMessage());
+        }
+        finally
+        {
+            owner.notifyConnectionClosed(this);
+        }
     }
 
     @Override
     public void close() throws IOException
     {
-        connection.close();
-        owner.notifyConnectionClosed(this);
+        connection.shutdownInput();
     }
 
     public boolean isClosed()
@@ -86,5 +157,11 @@ public class ClientConnection implements Runnable, Closeable
     public String getRemoteAddress()
     {
         return remoteAddress;
+    }
+
+    private void writeError(String msg)
+    {
+        out.println("E " + msg);
+        out.flush();
     }
 }
